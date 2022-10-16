b2f44b90ce2a3f2e5d8abded6fdb49b621f02e0a
Binh Vu <binh@toan2.com>
2022-10-16 14:00:04 -0700

    add test for resolve_type_arguments (which is used to determine param class of actor)

---
+++ tests/test_helper.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from typing import List, Literal
+
+from ream.actors.base import BaseActor
+from ream.helper import resolve_type_arguments
+
+
+@dataclass
+class CanGenParams:
+    # type of query that will be sent to ElasticSearch
+    query_type: Literal["exact-match", "fuzzy-match"]
+
+
+class CandidateGeneration(BaseActor[List[str], CanGenParams]):
+    VERSION = 100
+
+
+def test_resolve_type_arguments():
+    assert resolve_type_arguments(BaseActor, CandidateGeneration) == (
+        List[str],
+        CanGenParams,
+    )

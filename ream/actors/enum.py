08a433c749aadf7213b6d4d7aea870506f7127ee
Binh Vu <binh@toan2.com>
2023-06-12 22:52:58 -0700

    add enum actor

--- ream/actors/enum.py
+++ ream/actors/enum.py
@@ -1,18 +1,33 @@
 from __future__ import annotations
-from typing import Generic, TypeVar
+
+from abc import ABC, abstractmethod
+from typing import Generic, List, Optional, TypeVar, cast
+
 from ream.actor_state import ActorState
 from ream.actors.base import BaseActor, P
 from ream.params_helper import EnumParams
 
+
+class EnumInterface(ABC):
+    @abstractmethod
+    def exec(self, *args, **kwargs):
+        pass
+
+    def get_provenance(self) -> str:
+        return ""
+
+
 EP = TypeVar("EP", bound=EnumParams)
+EI = TypeVar("EI", bound=EnumInterface)  # enum interface
 
 
-class EnumActor(BaseActor[EP]):
-    def get_actor_state(self) -> ActorState:
-        deps = [actor.get_actor_state() for actor in self.dep_actors]
+class EnumActor(Generic[EP, EI], BaseActor[EP]):
+    def __init__(self, params: EP, dep_actors: Optional[List[BaseActor]] = None):
+        super().__init__(params, dep_actors)
 
+        # extract the method
         enum_fields = self.params.get_method_fields()
-        if not len(enum_fields) != 1:
+        if len(enum_fields) != 1:
             raise ValueError(
                 f"EnumActor must have exactly one method field, got {len(enum_fields)}"
             )
@@ -20,8 +35,12 @@ class EnumActor(BaseActor[EP]):
         enum_field = enum_fields[0]
         cls = self.params.get_method_class(enum_field)
         params = self.params.get_method_params(enum_field)
-        if params is None:
-            raise ValueError(
-                f"Parameter for an enum variant {cls} must be provided, got None"
-            )
-        return ActorState.create(cls, params, dependencies=deps)
+
+        # construct the selected method
+        self.method = cast(EI, cls(params))
+
+    def get_provenance(self):
+        return self.method.get_provenance()
+
+    def exec(self, *args, **kwargs):
+        return self.method.exec(*args, **kwargs)
